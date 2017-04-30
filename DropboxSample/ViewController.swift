7961aa1cd503f10b1bcc3b91fad4537ff7e7c743
IOKA <hit0apps@gmail.com>
2017-05-01 00:44:16 +0900

    download file from dropbox

--- DropboxSample/ViewController.swift
+++ DropboxSample/ViewController.swift
@@ -54,6 +54,33 @@ class ViewController: UIViewController {
         })
     }
     
+    fileprivate func download(filename: String) {
+        guard let client = DropboxClientsManager.authorizedClient else {
+            self.login()
+            return
+        }
+        
+        let destination: (URL, HTTPURLResponse) -> URL = { temporaryURL, response in
+            let tempPath = (NSTemporaryDirectory() as NSString).appendingPathComponent(filename)
+            return URL(fileURLWithPath: tempPath)
+        }
+        
+        client.files.download(path: "/\(filename)", destination: destination)
+            .response(completionHandler: {(response, error) in
+                guard let result = response else {
+                    if let error = error {
+                        print(error)
+                    }
+                    return
+                }
+                
+                print(result)
+            })
+            .progress({ progressData in
+                print(progressData)
+            })
+    }
+    
     fileprivate class func string(from date: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyyMMdd-HHmmss"
@@ -109,6 +136,8 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)!
+        self.download(filename: cell.textLabel!.text!)
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
